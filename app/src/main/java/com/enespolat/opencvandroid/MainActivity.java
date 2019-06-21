25dee94869685d58862ed8724657d5994f175037
Enes POLAT <turksancaktar1071@gmail.com>
2019-06-21 15:35:07 +0300

    Initial commit

---
+++ app/src/main/java/com/enespolat/opencvandroid/MainActivity.java
@@ -0,0 +1,46 @@
+package com.enespolat.opencvandroid;
+
+import android.graphics.Bitmap;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
+
+import java.io.IOException;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        OpenCVLoader.initDebug();
+    }
+    public void CannyEdge(View v){
+        Mat img = null;
+
+        try {
+            img = Utils.loadResource(getApplicationContext(), R.drawable.resim);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Imgproc.cvtColor(img, img, Imgproc.COLOR_RGB2BGRA);
+
+        Mat img_result = img.clone();
+        Imgproc.Canny(img, img_result, 80, 90);
+        Bitmap img_bitmap = Bitmap.createBitmap(img_result.cols(), img_result.rows(),Bitmap.Config.ARGB_8888);
+        Utils.matToBitmap(img_result, img_bitmap);
+        ImageView imageView = findViewById(R.id.img2);
+        imageView.setVisibility(View.VISIBLE);
+        imageView.setMinimumWidth(img.width());
+        imageView.setImageBitmap(img_bitmap);
+    }
+}
